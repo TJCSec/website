49a5a0285e1b601084663665d8ba34d95a21346c
Nathaniel Kerr <nathankerr1225@gmail.com>
2024-11-30 23:21:09 -0500

    Revert and use secrets correctly

--- update.py
+++ update.py
@@ -1,8 +1,8 @@
 from websocket import WebSocket
-import os
+import sys
 
 conn = WebSocket()
-conn.connect("wss://director.tjhsst.edu/sites/399/terminal/?", cookie=f"csrftoken={os.environ["CSRF_TOKEN"]};sessionid={os.environ["SESSION_ID"]}")
+conn.connect("wss://director.tjhsst.edu/sites/399/terminal/?", cookie=f"csrftoken={sys.argv[1]};sessionid={sys.argv[2]}")
 if 'true' not in conn.recv():
     print("Could not connect")
     exit(1)
